7f71fb51b4bf70c6fb0afcdeaaa1bcb048d1289a
Thassilo Schulze <thassilo@thasso.xyz>
2024-05-26 10:56:17 +0200

    Fix 64-bit GDT fields
    See page 98 of Volume 2 of the AMD64 Architecture Programmer's Manual:
    
    > If the processor is running in 64-bit mode (L=1), the only valid
    > setting of the D bit is 0.

--- bootloader/gdt64.s
+++ bootloader/gdt64.s
@@ -1,7 +1,7 @@
         ;; See gdt32.s for comments. This GDT is identical to the one for 32-bit
         ;; protected/long mode except that 64-bit-specific features are turned on.
 
-        align 8
+        align 16
 gdt64_start:
         ;; 8-byte null descriptor (index 0).
         ;; Used to catch translations with a null selector.
@@ -23,9 +23,9 @@ gdt64_code_segment:
         ;; 0-3: last 4 bits of segment limit
         ;;   4: unused (available for use by system software)
         ;;   5: 64-bit code segment flag indicates that this segment contains 64-bit code
-        ;;   6: default operation size of 32 bits
+        ;;   6: must be zero if L bit (bit 5) is set
         ;;   7: granularity of 4 kilobyte units
-        db 11101111b
+        db 10101111b
         ;; Last 8 bits of segment base address
         db 0x00
 
@@ -36,7 +36,7 @@ gdt64_data_segment:
         db 0x00
         ;; 0-3: segment type that specifies a read/write data segment
         db 10010010b
-        db 11101111b
+        db 10101111b
         dw 0x00
 
 gdt64_end:
