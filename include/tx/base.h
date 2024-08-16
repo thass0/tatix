3ba337506e71dabf44b4fda2c227e3ea06e4afd3
Thassilo Schulze <thassilo@thasso.xyz>
2024-08-16 09:08:15 +0200

    include: Clean up base.h header

--- include/tx/base.h
+++ include/tx/base.h
@@ -9,37 +9,82 @@
 #error "This kernel is written for the x86_64 architecture"
 #endif
 
-#define NULL ((void *)0)
+////////////////////////////////////////////////////////////////////////////////
+// Base types                                                                 //
+////////////////////////////////////////////////////////////////////////////////
 
-// Use predefined GCC macros to get the types for the correct sizes
-// (see https://gcc.gnu.org/onlinedocs/cpp/Common-Predefined-Macros.html).
-typedef __INT8_TYPE__ i8;
-typedef __INT16_TYPE__ i16;
-typedef __INT32_TYPE__ i32;
-typedef __INT64_TYPE__ i64;
-typedef __UINT8_TYPE__ u8;
-typedef __UINT16_TYPE__ u16;
-typedef __UINT32_TYPE__ u32;
-typedef __UINT64_TYPE__ u64;
+// We make extensive use of GCC predefined macros here:
+// https://gcc.gnu.org/onlinedocs/cpp/Common-Predefined-Macros.html
+
+typedef unsigned char       byte;
+typedef __UINT8_TYPE__      u8;
+typedef __UINT16_TYPE__     u16;
+typedef __UINT32_TYPE__     u32;
+typedef __UINT64_TYPE__     u64;
+typedef __INT8_TYPE__       i8;
+typedef __INT16_TYPE__      i16;
+typedef __INT32_TYPE__      i32;
+typedef __INT64_TYPE__      i64;
 // We only do signed pointer and size types
-typedef __INTPTR_TYPE__ ptr;
-typedef __UINTPTR_TYPE__ uptr;
-typedef __PTRDIFF_TYPE__ sz;
+typedef __INTPTR_TYPE__     ptr;
+typedef __UINTPTR_TYPE__    uptr;
+typedef __PTRDIFF_TYPE__    sz;
+typedef __SIZE_TYPE__       usz;
+typedef u8                  bool;
+
+#if __SIZEOF_FLOAT__ == 4
 typedef float f32;
+#else // __SIZEOF_FLOAT__ != 4
+#error "Expected float to by four bytes wide"
+#endif // __SIZEOF_FLOAT__ != 4
+
+#if __SIZEOF_DOUBLE__ == 8
 typedef double f64;
-typedef u8 bool; // Don't care what the C++ compiler thinks of this
+#else // __SIZEOF_DOUBLE__ != 8
+#error "Expected double eto be eight bytes wide"
+#endif // __SIZEOF_DOUBLE__ != 8
 
-#define true 1
-#define false 0
+////////////////////////////////////////////////////////////////////////////////
+// Limits                                                                     //
+////////////////////////////////////////////////////////////////////////////////
 
-#define sizeof(x) ((sz)sizeof(x))
-#define countof(arr) (sizeof(arr) / sizeof(*(arr))) // Number of elements in array
-#define lengthof(str) (countof(str) - 1) // Number of characters in string (excluding NULL byte)
+#define BYTE_MAX    __UINT8_MAX__
+#define U8_MAX      __UINT8_MAX__
+#define U16_MAX     __UINT16_MAX__
+#define U32_MAX     __UINT32_MAX__
+#define U64_MAX     __UINT64_MAX__
+#define I8_MAX      __INT8_MAX__
+#define I16_MAX     __INT16_MAX__
+#define I32_MAX     __INT32_MAX__
+#define I64_MAX     __INT64_MAX__
+#define PTR_MAX     __INTPTR_MAX__
+#define UPTR_MAX    __UINTPTR_MAX__
+#define SZ_MAX      __PTRDIFF_MAX__
+#define USZ_MAX     __SIZE_MAX__
 
-#define HLT()                                   \
-    do {                                        \
-        while (true)                            \
-            __asm__ volatile ("hlt");           \
-    } while (0);
+////////////////////////////////////////////////////////////////////////////////
+// Fundamental macros                                                         //
+////////////////////////////////////////////////////////////////////////////////
+
+// Make sure to enable -Wno-keyword-macro so this doesn't annoy you
+#define sizeof(x)       ((sz)sizeof(x))
+#define countof(arr)    (sizeof(arr) / sizeof((arr)[0]))
+#define lengthof(str)   (countof(str) - 1)
+#define NULL            ((void *)0)
+#define true            1
+#define false           0
+#define STRINGIFY(x)    #x
+#define MAX(a, b)       ((a) > (b) ? (a) : (b))
+#define MIN(a, b)       ((a) < (b) ? (a) : (b))
+
+////////////////////////////////////////////////////////////////////////////////
+// Variadic functions                                                         //
+////////////////////////////////////////////////////////////////////////////////
+
+#define va_start(v,l)       __builtin_va_start(v,l)
+#define va_end(v)           __builtin_va_end(v)
+#define va_arg(v,l)         __builtin_va_arg(v,l)
+#define va_copy(d,s)        __builtin_va_copy(d,s)
+typedef __builtin_va_list   va_list;
 
 #endif // __TX_BASE_H__
