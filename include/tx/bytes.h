71a6719cc49da8447a1a67a556f1f5b92b40cfe3
Thassilo Schulze <thassilo@thasso.xyz>
2024-08-24 08:56:29 +0200

    bytes: Add structure for a memory range of bytes
    `bytes` will be where we implement all raw memory-related
    functions. It might seems overly complicated to create a
    new struct for this when we have `struct str` already, but
    it's important to express intent clearly

---
+++ include/tx/bytes.h
@@ -0,0 +1,34 @@
+#ifndef __TX_BYTES_H__
+#define __TX_BYTES_H__
+
+#include <tx/base.h>
+#include <tx/string.h>
+
+struct bytes {
+    byte *dat;
+    sz len;
+};
+
+static inline struct bytes bytes_new(void *dat, sz len)
+{
+    struct bytes bytes;
+    bytes.dat = dat;
+    bytes.len = len;
+    return bytes;
+}
+
+static inline struct bytes bytes_from_str(struct str str)
+{
+    struct bytes bytes;
+    bytes.dat = (byte *)str.dat;
+    bytes.len = str.len;
+    return bytes;
+}
+
+static inline void memset(struct bytes bytes, byte value)
+{
+    for (sz i = 0; i < bytes.len; i++)
+        bytes.dat[i] = value;
+}
+
+#endif // __TX_BYTES_H__
