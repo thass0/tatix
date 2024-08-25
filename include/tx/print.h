5eb1168f6d34517b6f5ee00464089b9501db78f6
Thassilo Schulze <thassilo@thasso.xyz>
2024-08-25 16:05:39 +0200

    print: Add function for explicit debug printing

--- include/tx/print.h
+++ include/tx/print.h
@@ -6,6 +6,14 @@
 
 int print_str(struct str str);
 int print_fmt(struct str_buf buf, struct str fmt, ...);
-int print(struct str fmt, ...);
+
+#if __DEBUG__ >= 1
+int __print_dbg(struct str basename, sz lineno, struct str funcname, struct str fmt, ...);
+#define print_dbg(fmt, ...)                  \
+    __print_dbg(STR(__BASENAME__), __LINE__, \
+                (struct str){ .dat = DECONST(char *, __func__), .len = lengthof(__func__) }, fmt, ##__VA_ARGS__)
+#else
+#define print_dbg(fmt, ...)
+#endif
 
 #endif // __TX_PRINT_H__
--- src/print.c
+++ src/print.c
@@ -22,17 +22,22 @@ int print_fmt(struct str_buf buf, struct str fmt, ...)
     return rc;
 }
 
-int print(struct str fmt, ...)
+#if __DEBUG__ >= 1
+int __print_dbg(struct str basename, sz line, struct str funcname, struct str fmt_str, ...)
 {
     va_list argp;
     int rc;
-    char underlying[128];
+    char underlying[256];
     struct str_buf buf = str_buf_new(underlying, 0, countof(underlying));
-    va_start(argp, fmt);
-    rc = vfmt(&buf, fmt, argp);
+    va_start(argp, fmt_str);
+    rc = fmt(&buf, STR("%s %s:%ld "), funcname, basename, line);
+    if (rc < 0)
+        return -1;
+    rc = vfmt(&buf, fmt_str, argp);
     if (rc < 0)
         return rc;
     rc = print_str(str_from_buf(buf));
     va_end(argp);
     return rc;
 }
+#endif
