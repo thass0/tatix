eba9fc32bf1abe44a994f7c2beb18e7dc8b765bd
Thassilo Schulze <thassilo@thasso.xyz>
2024-12-08 11:42:55 +0100

    error: Add error types
    The comment in the `error.h` file explains most of my thinking.
    I'll experiment with how this goes, but I think it's good to add
    this early on so I can be consistent about the error types in
    the whole system.

---
+++ include/tx/error.h
@@ -0,0 +1,101 @@
+#ifndef __TX_ERROR_H__
+#define __TX_ERROR_H__
+
+#include <tx/base.h>
+#include <tx/bytes.h>
+
+// Many languages now feature results and options as ways of returning a value
+// that indicates either a success and a valid return value, or an error along
+// with an error code. Implementing this properly requires sum types, which C
+// doesn't support. C also doesn't have type-level polymorphism, so we can't
+// create structures that are generic over the type of some field (e.g. the
+// types of the success and error values could be generics).
+//
+// Two patterns are commonly used in C programs:
+// 1. The `int` return value that's negative on error and contains, e.g., and `errno`.
+// 2. An error pointer that also contains either an error or a pointer
+//
+// The downsides of each of these is that at the type-level, it's not visible
+// that some values are potential errors while other aren't. It is also easy
+// to ignore possible error values or forget checks. Additionally, just from
+// looking at the function interface, there is no way to know what values are
+// errors and what values are valid.
+//
+// The `struct result` type aims to be a middle-ground. It contains fields indicating
+// if there has been an error, an error code, and, possibly, a value that's returned on
+// success. To make the structure generic over the type of the return value, a macro
+// is provided that defines the struct along with it's default functions for any type.
+//
+// This is likely less performant than using the aforementioned methods of error handling.
+// However, it's conceivable to optimize some common cases like pointers with niching.
+// Regardless, clarity and safety should come first, and once both are established and
+// code has been written, the performance of this code can be measured and the
+// implementation of the result type can be optimized.
+
+// This is the case where return value is provided on success.
+struct result {
+    bool is_error;
+    u16 code;
+};
+
+static_assert(sizeof(struct result) == 4);
+
+static inline struct result result_error(u16 code)
+{
+    struct result r;
+    r.is_error = true;
+    r.code = code;
+    return r;
+}
+
+static inline struct result result_ok(void)
+{
+    struct result r;
+    r.is_error = false;
+    r.code = 0;
+    return r;
+}
+
+// NOTE: `struct_result(name, type)` can only be used in contexts, where `assert` is availble.
+// This is essentially everywhere, except in the functions used to implement `assert`.
+
+#define struct_result(name, type)                                                          \
+    struct result_##name {                                                                 \
+        bool is_error;                                                                     \
+        u16 code;                                                                          \
+        type unchecked_result_value;                                                       \
+    };                                                                                     \
+                                                                                           \
+    static inline type result_##name##_checked(struct result_##name r)                     \
+    {                                                                                      \
+        assert(!r.is_error);                                                               \
+        return r.unchecked_result_value;                                                   \
+    }                                                                                      \
+                                                                                           \
+    static inline struct result_##name result_##name##_error(u16 code)                     \
+    {                                                                                      \
+        struct result_##name r;                                                            \
+        memset(bytes_new(&r.unchecked_result_value, sizeof(r.unchecked_result_value)), 0); \
+        r.is_error = true;                                                                 \
+        r.code = code;                                                                     \
+        return r;                                                                          \
+    }                                                                                      \
+                                                                                           \
+    static inline struct result_##name result_##name##_ok(type t)                          \
+    {                                                                                      \
+        struct result_##name r;                                                            \
+        r.is_error = false;                                                                \
+        r.code = 0;                                                                        \
+        r.unchecked_result_value = t;                                                      \
+        return r;                                                                          \
+    }
+
+// Error codes.
+// These are the errno values because these are well-known and seem exhaustive enough.
+// For example, see https://en.wikipedia.org/wiki/Errno.h
+
+#define EIO 5
+#define ENOMEM 12
+#define EINVAL 22
+
+#endif // __TX_ERROR_H__
