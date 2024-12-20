ea48d8b62ba4f4b841573968c7ab37420fa23c1f
Thassilo Schulze <thassilo@thasso.xyz>
2024-12-20 21:58:00 +0100

    syscall: Add impl for read syscall
    This simply reads characters from the COM port until a
    CRLF sequence is found.

--- src/init_proc.s
+++ src/init_proc.s
@@ -2,15 +2,21 @@
 	global _init_proc
 
 _init_proc:
-	; mov rax, 0xcafe
-	; mov rbx, 0xcafe
-	mov dx, 0x3f8
-	mov ax, 'X'
-	out dx, ax
-	mov rax, 1 ; Syscall write
-	mov rdi, 0 ; fd is not used
-	mov rsi, msg ; rsi holds the pointer to the string
-	mov rdx, len ; rdx holds the length of the string in bytes
+    mov dx, 0x3f8
+    mov ax, 'X'
+    out dx, ax
+    ;; Let's write something
+    mov rax, 1 ; Syscall number write
+    mov rdi, 0 ; fd is not used
+    mov rsi, msg ; rsi holds the pointer to the string
+    mov rdx, msg_len ; rdx holds the length of the string in bytes
+    int 0x80
+
+    ;; Now we read some input
+    mov rax, 0 ; Syscall number read
+    ;; rdi still contains the unused fd numebr
+    mov rsi, buf ; rsi holds the buffer to read to
+    mov rdx, buf_len ; rdx holds the number of bytes to read
     int 0x80
 
 loop:
@@ -18,5 +24,9 @@ loop:
     jmp loop
 
 msg:
-    db "Hello, I am a user-space program", 10
-len equ $ - msg
+    db "Please enter your name:", 10
+msg_len equ $ - msg
+
+buf_len equ 256
+buf:
+    times buf_len db 0
--- src/syscall.c
+++ src/syscall.c
@@ -1,4 +1,6 @@
+#include <tx/com.h>
 #include <tx/error.h>
+#include <tx/fmt.h>
 #include <tx/print.h>
 #include <tx/syscall.h>
 
@@ -11,11 +13,49 @@ static struct result handle_syscall_write(i32 fd __unused, char *buf, sz len)
     return print_str(str_new(buf, len));
 }
 
+static struct result handle_syscall_read(i32 fd __unused, char *buf, sz len)
+{
+    if (!buf)
+        return result_error(EINVAL);
+    if (len < 0)
+        return result_error(EINVAL);
+
+    // Nothing to read in this case:
+    if (!len)
+        return result_ok();
+
+    struct result res = result_ok();
+    struct str_buf sbuf = str_buf_new(buf, 0, len);
+    char backing = '\0';
+    struct str_buf tmp_buf = str_buf_new(&backing, 0, 1);
+
+    do {
+        res = com_read(COM1_PORT, &tmp_buf);
+        if (res.is_error)
+            return res;
+        res = com_write(COM1_PORT, str_from_buf(tmp_buf));
+        if (res.is_error)
+            return res;
+        if (backing == '\r') {
+            res = com_write(COM1_PORT, STR("\n"));
+            if (res.is_error)
+                return res;
+        }
+        res = append_char(backing, &sbuf);
+        if (res.is_error)
+            return res;
+        str_buf_clear(&tmp_buf);
+    } while (backing != '\n' && backing != '\r');
+
+    return res;
+}
+
 void handle_syscall(struct trap_frame *cpu_state)
 {
     struct result res = result_ok();
     switch (cpu_state->rax) {
     case SYSCALL_NUM_READ:
+        res = handle_syscall_read(cpu_state->rdi, (char *)cpu_state->rsi, cpu_state->rdx);
         break;
     case SYSCALL_NUM_WRITE:
         res = handle_syscall_write(cpu_state->rdi, (char *)cpu_state->rsi, cpu_state->rdx);
