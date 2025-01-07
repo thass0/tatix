831b4ca7d6c0ae26f7e0a3dbf2b7a5159f7bb349
Thassilo Schulze <thassilo@thasso.xyz>
2025-01-07 17:47:59 +0100

    string: Add `str_is_equal` and `str_find_char` functions

--- include/tx/string.h
+++ include/tx/string.h
@@ -2,6 +2,7 @@
 #define __TX_STRING_H__
 
 #include <tx/assert.h>
+#include <tx/option.h>
 #include <tx/stringdef.h>
 
 static inline char str_buf_get_checked(struct str_buf *buf, sz idx)
@@ -17,6 +18,30 @@ static inline void str_buf_pop(struct str_buf *buf)
     assert(buf);
     if (buf->len > 0)
         buf->len--;
+}
+
+static inline bool str_is_equal(struct str a, struct str b)
+{
+    bool not_equal = false;
+    if (a.len != b.len)
+        return false;
+    while (a.len--) {
+        not_equal |= *a.dat != *b.dat;
+        a.dat++;
+        b.dat++;
+    }
+    return !not_equal;
+}
+
+static inline struct option_sz str_find_char(struct str s, char ch)
+{
+    sz l = s.len;
+    while (s.len) {
+        if (*s.dat++ == ch)
+            return option_sz_ok(l - s.len);
+        s.len--;
+    }
+    return option_sz_none();
 }
 
 #endif // __TX_STRING_H__
