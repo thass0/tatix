2e6e22340abb8feb799d08f49b04b1a345ae373a
Thassilo Schulze <thassilo@thasso.xyz>
2025-05-03 13:35:19 +0200

    netdev: Add IP address to `netdev`
    Each network device is now assigned an IP address. This allows passing
    the IP address that a frame was received on to the upper layers above
    the drvier. Each network device could be assigned a differnet IP address
    although, at present, it's just one default IP addres for all devices
    (and in reality there is only one device).

--- include/tx/netdev.h
+++ include/tx/netdev.h
@@ -6,8 +6,8 @@
 #include <tx/arena.h>
 #include <tx/byte.h>
 #include <tx/error.h>
-#include <tx/mac.h>
 #include <tx/ip.h>
+#include <tx/mac.h>
 
 struct netdev;
 
@@ -15,21 +15,29 @@ typedef struct result (*send_frame_func_t)(struct netdev *dev, struct byte_view 
 
 struct netdev {
     struct mac_addr mac_addr;
+    struct ipv4_addr ip_addr;
     send_frame_func_t send_frame;
     void *private_data;
 };
 
+// Set the `netdev` subsystem with a default IP address to use for all new devices.
+void netdev_set_default_ip_addr(struct ipv4_addr ip_addr);
+
 // Send a frame (only Ethernet right now) with data `frame` using the device with MAC address `addr`.
 struct result netdev_send_frame(struct mac_addr addr, struct byte_view frame);
 
-// Perform an ARP scan by broadcasting an ARP REQUEST for the device with IPv4 address `dest_ip` from each of the
-// registered network devices. `src_ip` is the IPv4 address of this computer (the host) and `dest_ip` is the IPv4
-// address that we want to know the MAC address for. `arn` is used for temporary storage.
-struct result netdev_arp_scan(struct ipv4_addr src_ip, struct ipv4_addr dest_ip, struct arena arn);
+// Perform an ARP scan by broadcasting an ARP request for the device with IPv4 address `ip_addr` from each of the
+// registered network devices. `ip_addr` is the IPv4 address that we want to know the MAC address for. The IP addresses
+// assigned to the individual network devices will be used as the source IP addresses in the ARP requests. `arn` is
+// used for temporary storage.
+struct result netdev_arp_scan(struct ipv4_addr ip_addr, struct arena arn);
 
 // Register a `struct netdev` network device with the `netdev` subsystem. The `mac_addr` field in the given structure
 // `dev` will be used to look up the device. `send_frame` will be called to send a frame over the device. `private_data`
-// could, for example, be the driver-specific structure of a network driver that it needs to function.
+// could, for example, be the driver-specific structure of a network driver that it needs to function. The `ip_addr`
+// field must be 0.0.0.0, indicating that the default IP address can be assigned.
+//
+// The memory behind the `dev` pointer passed to this function is owned by the driver.
 //
 // This function should be called by network device drivers to make themselves available for sending frames after they
 // have initialized.
@@ -38,6 +46,6 @@ struct result netdev_arp_scan(struct ipv4_addr src_ip, struct ipv4_addr dest_ip,
 // The way to receive data is for the driver to call directly into the Ethernet implementation (or, if there are
 // other data link layer protocols implemented in the future, the respective drivers could call directly into the
 // respective implementations).
-struct result netdev_register_device(struct netdev dev);
+struct result netdev_register_device(struct netdev *dev);
 
 #endif // __TX_NETDEV_H__
--- src/netdev.c
+++ src/netdev.c
@@ -4,17 +4,37 @@
 
 #define NETDEV_TABLE_SIZE 16
 static bool global_netdev_table_used[NETDEV_TABLE_SIZE];
-static struct netdev global_netdev_table[NETDEV_TABLE_SIZE];
+static struct netdev *global_netdev_table[NETDEV_TABLE_SIZE];
 
-struct result netdev_register_device(struct netdev dev)
+static struct ipv4_addr global_netdev_default_ip_addr;
+
+void netdev_set_default_ip_addr(struct ipv4_addr ip_addr)
 {
-    byte mac_addr_fmt_buf[2 * MAC_ADDR_FMT_BUF_SIZE];
-    struct arena mac_addr_fmt_arn = arena_new(byte_array_new(mac_addr_fmt_buf, countof(mac_addr_fmt_buf)));
+    global_netdev_default_ip_addr = ip_addr;
+}
+
+struct result netdev_register_device(struct netdev *dev)
+{
+    assert(dev);
+
+    struct ipv4_addr zero = ipv4_addr_new(0, 0, 0, 0);
+
+    if (ipv4_addr_is_equal(global_netdev_default_ip_addr, zero))
+        return result_error(EINVAL);
+
+    // The device isn't allowed to set a custom IP address:
+    if (!ipv4_addr_is_equal(dev->ip_addr, zero))
+        return result_error(EINVAL);
+
+    dev->ip_addr = global_netdev_default_ip_addr;
+
+    byte fmt_buf[2 * MAC_ADDR_FMT_BUF_SIZE + IP_ADDR_FMT_BUF_SIZE];
+    struct arena fmt_arn = arena_new(byte_array_new(fmt_buf, countof(fmt_buf)));
 
     for (sz i = 0; i < NETDEV_TABLE_SIZE; i++) {
-        if (global_netdev_table_used[i] && mac_addr_is_equal(dev.mac_addr, global_netdev_table[i].mac_addr)) {
+        if (global_netdev_table_used[i] && mac_addr_is_equal(dev->mac_addr, global_netdev_table[i]->mac_addr)) {
             print_dbg(PDBG, STR("Device with MAC address %s already exists\n"),
-                      mac_addr_format(dev.mac_addr, &mac_addr_fmt_arn));
+                      mac_addr_format(dev->mac_addr, &fmt_arn));
             return result_error(EEXIST);
         }
     }
@@ -23,8 +43,8 @@ struct result netdev_register_device(struct netdev dev)
         if (!global_netdev_table_used[i]) {
             global_netdev_table_used[i] = true;
             global_netdev_table[i] = dev;
-            print_dbg(PINFO, STR("Registered device with MAC address %s\n"),
-                      mac_addr_format(dev.mac_addr, &mac_addr_fmt_arn));
+            print_dbg(PINFO, STR("Registered device with MAC address %s and IP address %s\n"),
+                      mac_addr_format(dev->mac_addr, &fmt_arn), ipv4_addr_format(dev->ip_addr, &fmt_arn));
             return result_ok();
         }
     }
@@ -35,8 +55,8 @@ struct result netdev_register_device(struct netdev dev)
 static struct netdev *netdev_lookup_mac_addr(struct mac_addr addr)
 {
     for (sz i = 0; i < NETDEV_TABLE_SIZE; i++) {
-        if (global_netdev_table_used[i] && mac_addr_is_equal(addr, global_netdev_table[i].mac_addr)) {
-            return &global_netdev_table[i];
+        if (global_netdev_table_used[i] && mac_addr_is_equal(addr, global_netdev_table[i]->mac_addr)) {
+            return global_netdev_table[i];
         }
     }
 
@@ -53,14 +73,14 @@ struct result netdev_send_frame(struct mac_addr addr, struct byte_view frame)
     return dev->send_frame(dev, frame);
 }
 
-struct result netdev_arp_scan(struct ipv4_addr src_ip, struct ipv4_addr dest_ip, struct arena arn)
+struct result netdev_arp_scan(struct ipv4_addr ip_addr, struct arena arn)
 {
     struct result res = result_ok();
 
     for (sz i = 0; i < NETDEV_TABLE_SIZE; i++) {
         if (global_netdev_table_used[i]) {
-            struct netdev *dev = &global_netdev_table[i];
-            res = arp_send_request(src_ip, dev->mac_addr, dest_ip, arn);
+            struct netdev *dev = global_netdev_table[i];
+            res = arp_send_request(dev->ip_addr, dev->mac_addr, ip_addr, arn);
             if (res.is_error)
                 return res;
         }
