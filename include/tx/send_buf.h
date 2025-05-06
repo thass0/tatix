f8ef77ce839acd4a2a26ba94e3a8c3b3a5db289b
Thassilo Schulze <thassilo@thasso.xyz>
2025-05-06 17:23:07 +0200

    send_buf: Efficient buffer for constructing packets
    The send buffer is a data structure that aims the
    reduce the number of copy operations required to
    construct a packet. The implementation is explained
    in the code ;^)

---
+++ include/tx/send_buf.h
@@ -0,0 +1,37 @@
+// Efficient buffer for constructing packets to be sent over the network.
+//
+// This data structure aims to reduce the number of copy operations required to construct a packet. When going down the
+// layers of the protocol stack, protocol headers must often be _prepended_ to an existing buffer of data. This data
+// structure allows constructing each protocol header exactly once without copying it again until the final packet is
+// copied into the memory of a network driver.
+
+#ifndef __TX_SEND_BUF_H__
+#define __TX_SEND_BUF_H__
+
+#include <tx/arena.h>
+#include <tx/base.h>
+#include <tx/byte.h>
+
+#define SEND_BUF_NUM_PARTS 8
+
+struct send_buf {
+    struct arena arn;
+    struct byte_buf parts[SEND_BUF_NUM_PARTS];
+    sz n_used;
+};
+
+// Create a new send buffer that uses `arn` for its underlying memory.
+struct send_buf send_buf_new(struct arena arn);
+
+// Get a new byte buffer from the send buffer. The bytes written the the new buffer will be prepended to the content of
+// all existing buffers in the send buffer when assembling the complete content of the send buffer.
+struct byte_buf *send_buf_prepend(struct send_buf *sb, sz buf_size);
+
+// Compute the total length of the send buffer's content. I.e., the length of the content appended the to the byte
+// buffer when calling `send_buf_assemble`.
+sz send_buf_total_length(struct send_buf sb);
+
+// Append the complete content of the send buffer to `buf`.
+struct result send_buf_assemble(struct send_buf sb, struct byte_buf *buf);
+
+#endif // __TX_SEND_BUF_H__
---
+++ src/send_buf.c
@@ -0,0 +1,66 @@
+#include <tx/send_buf.h>
+
+// NOTE: The complete content of the send buffer can be computed at any time by appending the content in the parts in
+// reverse order. Thus, to prepend data to a send buffer, simply get the next part from the array of parts and append
+// the data to this part.
+//
+// Each of the parts is allocated out of the arena allocator in the send buffer. The size of this arena allocator thus
+// limits the capacity of the send buffer. Advantages of this scheme are that allocators from arenas are fast and that
+// the memory behind all parts of a send buffer is in the same place.
+
+struct send_buf send_buf_new(struct arena arn)
+{
+    struct send_buf sb;
+    sb.arn = arn;
+    byte_array_set(byte_array_new(&sb.parts, sizeof(sb.parts)), 0);
+    sb.n_used = 0;
+    return sb;
+}
+
+struct byte_buf *send_buf_prepend(struct send_buf *sb, sz buf_size)
+{
+    assert(sb);
+
+    if (sb->n_used == SEND_BUF_NUM_PARTS)
+        return NULL;
+
+    struct byte_buf *buf = &sb->parts[sb->n_used];
+    *buf = byte_buf_from_array(byte_array_from_arena(buf_size, &sb->arn));
+    sb->n_used++;
+
+    return buf;
+}
+
+sz send_buf_total_length(struct send_buf sb)
+{
+    sz len = 0;
+
+    for (sz i = 0; i < sb.n_used; i++)
+        len += sb.parts[i].len;
+
+    return len;
+}
+
+struct result send_buf_assemble(struct send_buf sb, struct byte_buf *buf)
+{
+    assert(buf);
+
+    sz len_before = buf->len;
+
+    // Data is prepended to a send buffer by _appending_ it to the buffer `sb.parts[sb.n_used]` and then incrementing
+    // the `n_used` member so the next prepend operation uses the next buffer. Hence, to assemble the content of the
+    // send buffer in the right order, we need to append the content of all parts to `buf` starting with
+    // `sb.parts[sb.n_used - 1]` and working our way backwards.
+
+    for (sz i = sb.n_used; i > 0; i--) {
+        sz n_appended = byte_buf_append(buf, byte_view_from_buf(sb.parts[i - 1]));
+        if (n_appended != sb.parts[i - 1].len) {
+            return result_error(ENOMEM);
+        }
+    }
+
+    // To make sure we did this correctly. Callers rely on this invariant to be true.
+    assert(buf->len - len_before == send_buf_total_length(sb));
+
+    return result_ok();
+}
