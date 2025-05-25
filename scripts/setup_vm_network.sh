b374a62040f4077c57a12ac00916ab8d0f76b39c
Thassilo Schulze <thassilo@thasso.xyz>
2025-05-25 16:43:04 +0200

    scripts: Rename `$ETHER` to `$IF` in scripts
    This makes sense because the interface doesn't need
    to be an ethernet device. Wi-Fi also works for example.

--- scripts/setup_vm_network.sh
+++ scripts/setup_vm_network.sh
@@ -2,8 +2,8 @@
 
 source $(dirname $0)/vm_config.env
 
-if [ -z "$ETHER" ]; then
-	echo "Missing environment variable ETHER"
+if [ -z "$IF" ]; then
+    echo "Missing environment variable IF (outward-facing network interface)"
 	exit -1
 fi
 
@@ -25,15 +25,15 @@ sudo chmod u+rw /sys/class/net/$TAP
 
 sudo sysctl -w net.ipv4.ip_forward=1
 
-sudo iptables -t nat -A POSTROUTING -o $ETHER -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -o $IF -j MASQUERADE
 sudo iptables -A FORWARD -i $BRIDGE -j ACCEPT
 sudo iptables -A FORWARD -o $BRIDGE -m state --state RELATED,ESTABLISHED -j ACCEPT
 
 # Forward ICMP messages to the VM:
-sudo iptables -A FORWARD -d $VM_IP -i $ETHER -j ACCEPT
-sudo iptables -t nat -A PREROUTING -i $ETHER -p icmp -j DNAT --to-destination $VM_IP
+sudo iptables -A FORWARD -d $VM_IP -i $IF -j ACCEPT
+sudo iptables -t nat -A PREROUTING -i $IF -p icmp -j DNAT --to-destination $VM_IP
 
 echo "Network setup complete."
 echo "Bridge: $BRIDGE ($BRIDGE_IP)"
 echo "TAP device: $TAP"
-echo "Ethernet device: $ETHER"
+echo "Interface: $IF"
