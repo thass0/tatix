b8de9d8c4df46d6258047b9e27dd413c46e1d620
Thassilo Schulze <thassilo@thasso.xyz>
2025-05-25 16:43:04 +0200

    scripts: Rename `$ETHER` to `$IF` in scripts
    This makes sense because the interface doesn't need
    to be an ethernet device. Wi-Fi also works for example.

--- scripts/cleanup_vm_network.sh
+++ scripts/cleanup_vm_network.sh
@@ -2,8 +2,8 @@
 
 source $(dirname $0)/vm_config.env
 
-if [ -z "$ETHER" ]; then
-	echo "Missing environment variable ETHER"
+if [ -z "$IF" ]; then
+    echo "Missing environment variable IF (outward facing network interface)"
 	exit -1
 fi
 
@@ -16,10 +16,10 @@ sudo ip link delete $BRIDGE
 
 sudo sysctl -w net.ipv4.ip_forward=1
 
-sudo iptables -t nat -D POSTROUTING -o $ETHER -j MASQUERADE
+sudo iptables -t nat -D POSTROUTING -o $IF -j MASQUERADE
 sudo iptables -D FORWARD -i $BRIDGE -j ACCEPT
 sudo iptables -D FORWARD -o $BRIDGE -m state --state RELATED,ESTABLISHED -j ACCEPT
 
-sudo iptables -D FORWARD -d $VM_IP -i $ETHER -j ACCEPT
-sudo iptables -t nat -D PREROUTING -i $ETHER -p icmp -j DNAT --to-destination $VM_IP
+sudo iptables -D FORWARD -d $VM_IP -i $IF -j ACCEPT
+sudo iptables -t nat -D PREROUTING -i $IF -p icmp -j DNAT --to-destination $VM_IP
 
