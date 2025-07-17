49cbd3af47394991ee8111b7e7a8e7d20a05769d
Thassilo Schulze <thassilo@thasso.xyz>
2025-07-17 17:46:12 +0200

    pool: Switch from `/**/` to `//` comments
    I wrote the pool allocator early on before
    settling on the `//` comment style. Now it's
    updated.

--- include/tx/pool.h
+++ include/tx/pool.h
@@ -7,14 +7,12 @@
 #include <tx/byte.h>
 
 struct pool {
-    ptr *head; /* First block in the list */
-    sz size; /* Size of each block */
+    ptr *head;
+    sz size;
 };
 
-/**
- * Create a new pool. It uses `ba.dat` as its source of memory. The minimum
- * block size is `MAX(sizeof(ptr), alignof(void *))`.
- */
+// Create a new pool allocator that uses the byte array `ba` as its source of memory.
+// The minimum block size is `MAX(sizeof(ptr), alignof(void *))`
 static inline struct pool pool_new(struct byte_array ba, sz block_size)
 {
     struct pool pool = { .head = NULL, .size = 0 };
@@ -32,7 +30,6 @@ static inline struct pool pool_new(struct byte_array ba, sz block_size)
     assert(block_size > 0);
     n_blocks = ba.len / block_size;
 
-    /* Link all the blocks that are available */
     for (sz i = 0; i < n_blocks * block_size; i += block_size) {
         block = (ptr *)(ba.dat + i);
         *block = (ptr)pool.head;
@@ -44,9 +41,7 @@ static inline struct pool pool_new(struct byte_array ba, sz block_size)
     return pool;
 }
 
-/**
- * Allocate a block from the pool. Returns `NULL` if the pool is empty.
- */
+// Allocate a block from the pool. Returns `NULL` if the pool is empty.
 static inline void *pool_alloc(struct pool *pool)
 {
     ptr *block = NULL;
@@ -63,32 +58,13 @@ static inline void *pool_alloc(struct pool *pool)
     return block;
 }
 
-/**
- * Free a block back to the pool.
- */
+// Free a block from the pool.
 static inline void pool_free(struct pool *pool, void *block)
 {
     assert(pool);
-    assert(block);
 
     *(ptr *)block = (ptr)pool->head;
     pool->head = block;
 }
 
-/**
- * Allocate a pool from an arena.
- */
-static inline struct pool *pool_from_arena(sz n, sz size, struct arena *arn)
-{
-    assert(size > 0);
-    assert(n > 0);
-    assert(arn);
-    struct pool *pool = arena_alloc(arn, sizeof(*pool));
-    void *buf = arena_alloc_aligned_array(arn, n, size, size);
-    assert(buf);
-    assert(n <= SZ_MAX / size);
-    *pool = pool_new(byte_array_new(buf, n * size), size);
-    return pool;
-}
-
-#endif /* __TX_POOL_H__ */
+#endif // __TX_POOL_H__
