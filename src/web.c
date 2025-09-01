b2eab212a4990cde194c4daac9092936f42d6ef8
Thassilo Schulze <thassilo@thasso.xyz>
2025-09-01 17:19:36 +0200

    web: Add `Cache-Control: max-age=3600` header
    This way, we instruct browsers to cache the content we send
    it for a while. An hour should be fine for the kind of static
    sites Tatix is built to host.

--- src/web.c
+++ src/web.c
@@ -244,7 +244,15 @@ static struct result http_build_header(enum http_status status, enum http_conten
     // partially. This is neat.
     struct str_buf buf = str_buf_from_byte_buf(*response_buf);
 
-    struct result res = fmt(&buf, STR("HTTP/1.0 %u %s\r\n"), (u32)status, http_status_to_string(status));
+    struct result res = fmt(&buf, STR("HTTP/1.1 %u %s\r\n"), (u32)status, http_status_to_string(status));
+    if (res.is_error)
+        return res;
+
+    res = str_buf_append(&buf, STR("Connection: close\r\n"));
+    if (res.is_error)
+        return res;
+
+    res = str_buf_append(&buf, STR("Cache-Control: max-age=3600\r\n"));
     if (res.is_error)
         return res;
 
